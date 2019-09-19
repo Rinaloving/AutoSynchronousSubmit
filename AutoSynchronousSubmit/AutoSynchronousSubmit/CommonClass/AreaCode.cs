34337eae54e9fd3e7418fa607ba21ac1b7d31307
cfl <1243455143@qq.com>
2019-09-19 17:45:16 +0800

    添加区县枚举类

---
+++ AutoSynchronousSubmit/AutoSynchronousSubmit/CommonClass/AreaCode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSynchronousSubmit.CommonClass
+{
+    /// <summary>
+    /// 区县枚举
+    /// </summary>
+    public enum AreaCode
+    {
+        /// <summary>
+        /// 庆云
+        /// </summary>
+        [Description("庆云")]
+        QY = 371423,
+        /// <summary>
+        /// 齐河
+        /// </summary>
+        [Description("齐河")]
+        QH = 371425,
+        /// <summary>
+        /// 平原
+        /// </summary>
+        [Description("平原")]
+        PY =371426,
+        /// <summary>
+        /// 夏津
+        /// </summary>
+        [Description("夏津")]
+        XJ =371426,
+        /// <summary>
+        /// 德城区
+        /// </summary>
+        [Description("德城区")]
+        DC =371402,
+        /// <summary>
+        /// 陵城区
+        /// </summary>
+        [Description("陵城区")]
+        LC =371421,
+        /// <summary>
+        /// 宁津
+        /// </summary>
+        [Description("宁津")]
+        NJ =371422,
+        /// <summary>
+        /// 临邑
+        /// </summary>
+        [Description("临邑")]
+        LY =371424,
+        /// <summary>
+        /// 武城
+        /// </summary>
+        [Description("武城")]
+        WC =371428,
+        /// <summary>
+        /// 乐陵
+        /// </summary>
+        [Description("乐陵")]
+        LL=371481,
+        /// <summary>
+        /// 禹城
+        /// </summary>
+        [Description("禹城")]
+        YC =371482,
+
+    }
+}
