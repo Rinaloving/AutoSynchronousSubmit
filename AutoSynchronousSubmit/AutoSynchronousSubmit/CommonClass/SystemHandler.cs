3f85db0a5c3c25b50ded9a7b59c86878c77fa229
Rinaloving <1426429456@qq.com>
2019-09-20 13:40:50 +0800

    通过反射创建对象并为属性赋值

---
+++ AutoSynchronousSubmit/AutoSynchronousSubmit/CommonClass/SystemHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSynchronousSubmit.CommonClass
+{
+    public class SystemHandler
+    {
+
+        public static readonly string assPath = System.Windows.Forms.Application.StartupPath + "/Model.dll";
+    }
+}
--- AutoSynchronousSubmit/AutoSynchronousSubmit/SynchronousSubmit.cs
+++ AutoSynchronousSubmit/AutoSynchronousSubmit/SynchronousSubmit.cs
@@ -1,6 +1,7 @@
 ﻿using AutoSynchronousSubmit.CommonClass;
 using BLL.SubmitBll;
 using CCWin;
+using Model.SubmitModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,6 +9,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -39,21 +41,113 @@ namespace AutoSynchronousSubmit
 
             string path = @"D:\dzxml\371425\BizMsg";
 
-            string[] files = XMLHelper.GetBizFile(path);
-            foreach (var item in files)
-            {
-               var val =  XElement.Load(item).Element("Data").Nodes(); // 获取子节点值
-                foreach (var it in val)
-                {
-                    var val2 = it.; // 获取子节点值
-                }
-              
-                var result = "";
+            List<dynamic> entities = GetSmtInstance(path);
 
-            }
-            
+            var result = "";
+
 
 
         }
+
+        public List<dynamic> GetSmtInstance(string path)
+        {
+            List<dynamic> lst = new List<dynamic>();
+            string[] files = XMLHelper.GetBizFile(path);
+            foreach (var item in files)
+            {
+                Head head = XMLHelper.GetBizHeadInfo(item);
+                IEnumerable<XNode> val = XElement.Load(item).Element("Data").Nodes(); // 获取子节点值
+                foreach (XNode it in val)
+                {
+                    XElement val2 = (XElement)it; // 获取子节点值
+                    var currentNodeName = val2.Name; // 当前节点名
+                    IEnumerable<XAttribute> m = val2.Attributes();
+
+                    //反射获取表实例
+                    Assembly assembly = Assembly.LoadFrom(SystemHandler.assPath);
+                    dynamic entity = assembly.CreateInstance("Model.SubmitModel." + currentNodeName);
+                    PropertyInfo[] fields = entity.GetType().GetProperties(); // 获取指定对象的所有公共属性
+
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+
+                        foreach (XAttribute ia in m)
+                        {
+                            var value = ia.Value; //属性值
+                            var name = ia.Name; // 属性名
+                            //如果属性与表中属性相同则赋值给对象
+                            if (fields[i].Name.ToString() == name.LocalName)
+                            {
+                                fields[i].SetValue(entity, ConvertValueType(fields, value.ToString(), i)); // 赋值
+                                break;
+                            }
+                            
+
+
+                        }
+
+                        if (fields[i].Name.ToString() == "PID")
+                        {
+                            fields[i].SetValue(entity, ConvertValueType(fields, Guid.NewGuid().ToString(), i)); // 赋值
+                        }
+                        else if (fields[i].Name.ToString() == "QXDM")
+                        {
+                            fields[i].SetValue(entity, ConvertValueType(fields, head.AreaCode, i)); // 赋值
+                        }
+                        else if (fields[i].Name.ToString() == "CASENUM")
+                        {
+                            fields[i].SetValue(entity, ConvertValueType(fields, head.RecFlowID, i)); // 赋值
+                        }
+                        else if (fields[i].Name.ToString() == "CREATETIME")
+                        {
+                            fields[i].SetValue(entity, ConvertValueType(fields, head.CreateDate, i)); // 赋值
+                        }
+                         
+                    }
+
+
+                    lst.Add(entity);
+
+                }
+
+               
+
+            }
+            return lst;
+        }
+
+
+        public object ConvertValueType(PropertyInfo[] fields, object value,int i)
+        {
+            object result = null;
+            if (fields[i].PropertyType.FullName == "System.Int16")
+            {
+                result = Convert.ToDecimal(value);
+            }
+            else if (fields[i].PropertyType.FullName == "System.Double")
+            {
+                result = Convert.ToDecimal(value);
+            }
+            else if (fields[i].PropertyType.FullName == "System.Byte")
+            {
+                result = Convert.ToByte(value);
+            }
+            else if (fields[i].PropertyType.FullName == "System.Decimal")
+            {
+                result = Convert.ToDecimal(value);
+            }
+            else if (fields[i].PropertyType.FullName == "System.DateTime")
+            {
+                result = Convert.ToDateTime(value);
+            }
+            else
+            {
+                result = value;
+            }
+
+            return result;
+        }
+
+
     }
 }
